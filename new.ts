e6e299508ab18179888726814c761ada2d97ed99
chiennt43 <38646168+chiennt43@users.noreply.github.com>
2025-12-12 13:01:39 +0700

    test function

--- new.ts
+++ new.ts
@@ -2,4 +2,12 @@ const newStudent = {
     name: 'Han',
     age: 23,
     address: 'Nguyen Tri Phuong',
+    
+}
+
+const anh = () => {
+    const condition = true;
+    if (condition) {
+        console.log("OKOK");
+    }
 }
